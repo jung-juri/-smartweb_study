4b54addf7fd0b368796f93f55bb105daf3574ab8
jung-juri <gokui@naver.com>
2016-12-14 18:17:58 +0900

    dd

---
+++ TEST/jQuery시험문제/js/script.js
@@ -0,0 +1,11 @@
+//1번정답---------
+$(function(){
+  $('header > div > nav > ul > li > a').mouseover(function(){
+    $(this).addClass('hov')
+  }).mouseout(function(){
+    $(this).removeClass()
+  })
+})
+//1번정답---------
+
+//2번정답---------
